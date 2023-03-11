00da12a073a8629a6dc1cf20c9a47568c515466c
Anna An <arzumanyan.an@gmail.com>
2023-03-11 20:08:41 +0400

    adding signup endpoint

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,4 +1,6 @@
 import validator from "validator";
+import bcrypt from "bcrypt";
+import * as jose from "jose";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -55,8 +57,52 @@ export default async function handler(req, res) {
     if (errors.length) {
       return res.status(400).json({ errorMessage: errors[0] });
     }
+
+    // validating that the user doesn;t already have an account by checking the email in the db, in this case in the arrawy belows
+
+    // 40f08f6b58fe0f49ef1d22f1886aaa30   right
+    // ba2a56ba0c2dd5e593a8eb5d84afc7b9   wrong
+    const user = [
+      {
+        firstName: "Anna",
+        lastName: "An",
+        email: "anna@email.com",
+        phone: "111222333333",
+        city: "Seoul",
+        password: "somepasswodHere/",
+      },
+      {
+        firstName: "Ben",
+        lastName: "B",
+        email: "anna1@email.com",
+        phone: "111222333333",
+        city: "Seoul",
+        password: "somepasswodHere/",
+      },
+    ];
+
+    const userWithEmail = user.find((el) => el.email === email);
+    if (userWithEmail) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Email is already registered" });
+    }
+
+    // hashing the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create a jwt
+    const alg = "HS256";
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const token = await new jose.SignJWT({
+      email: email,
+    })
+      .setProtectedHeader({ alg })
+      .setExpirationTime("24H")
+      .sign(secret);
+
     res.status(200).json({
-      hello: "body",
+      hello: token,
     });
   }
 }
